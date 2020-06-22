ca05a5bd675c3e0988d87af1d9e3412eedaf8039
avsssai <34810623+avsssai@users.noreply.github.com>
2020-06-22 16:17:45 +0530

    Updates

---
+++ precache-manifest.3b576a7e7396f035424238731a22f8cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdb3bfd4c8b2a502e9b5471ad2ecc05",
+    "url": "/todoMVC/index.html"
+  },
+  {
+    "revision": "b42c42e881a73ff6d68b",
+    "url": "/todoMVC/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c9834dbe4295b3207551",
+    "url": "/todoMVC/static/css/main.e6d9324f.chunk.css"
+  },
+  {
+    "revision": "b42c42e881a73ff6d68b",
+    "url": "/todoMVC/static/js/2.29a8cbf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoMVC/static/js/2.29a8cbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9834dbe4295b3207551",
+    "url": "/todoMVC/static/js/main.8171eb61.chunk.js"
+  },
+  {
+    "revision": "971fe0a67b8edfa982b4",
+    "url": "/todoMVC/static/js/runtime-main.a36b93bb.js"
+  }
+]);
